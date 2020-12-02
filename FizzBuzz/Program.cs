24e4c15bb928c3967ee763f3e34c8ef7a24a9c75
Carlos Garcia <carlosagarciaii@gmail.com>
2020-12-02 16:50:08 -0600

    Initial Buils

---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using FizzBuzz_CL;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+            NumberLister nl = new NumberLister();
+
+            Console.WriteLine("Hello World!");
+            Console.WriteLine(nl.Counter(cMax : int.MaxValue) );
+        }
+    }
+}
---
+++ FizzBuzz_CL/NumberLister.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FizzBuzz_CL
+{
+    public class NumberLister
+    {
+        private const int Min = 1;
+        private const int Max = 100;
+        private int CurrentNumber ;
+        private string counterOutput;
+
+        public string Counter(int cMin = Min, int cMax= Max)
+        {
+            CurrentNumber = 0;
+            counterOutput = "";
+
+            
+
+            do
+            {
+                counterOutput += "\n";
+                CurrentNumber++;
+                if (CurrentNumber %3 == 0 && CurrentNumber %5 == 0)
+                {
+                    counterOutput += "Fizz - Buzz";
+                }
+                
+                else if(CurrentNumber % 3 == 0){
+                    counterOutput += "Fizz";                    
+                    }
+                else if (CurrentNumber %5 == 0)
+                {
+                    counterOutput += "Buzz";
+
+                }
+                else if(CurrentNumber == Min)
+                {
+                    counterOutput += Min.ToString();
+                }
+                else
+                {
+                    counterOutput += CurrentNumber.ToString();
+                }
+
+
+            }
+            while( CurrentNumber < cMax);
+            
+            return counterOutput;
+        }
+
+
+
+
+    }
+}
